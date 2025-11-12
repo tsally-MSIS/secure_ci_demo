059f9336e6ee02ca301aeb7b2862822d218dc060
NetAcad DEVASC <changeme@example.com>
2025-11-12 02:03:38 +0000

    Test 24

--- sample_code.py
+++ sample_code.py
@@ -20,5 +20,5 @@ def divide(a, b):
 if __name__ == "__main__":
     print("Addition of 3 and 3 is:", add(3, 3))
     print("Subtraction of 6 from 10 is:", subtract(10, 6))
-    print("Multiplication of 6 and 5 is:", multiply(6, 5))
+    print("Multiplication of 6 and 6 is:", multiply(6, 6))
     print("Division of 10 by 5 is:", divide(10, 5))
