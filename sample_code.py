20a7832ced27626f715383a9915f7b48d47cb997
NetAcad DEVASC <changeme@example.com>
2025-11-14 01:31:00 +0000

    Test 05

--- sample_code.py
+++ sample_code.py
@@ -22,5 +22,3 @@ if __name__ == "__main__":
     print("Subtraction of 5 from 10 is:", subtract(10, 5))
     print("Multiplication of 6 and 6 is:", multiply(6, 6))
     print("Division of 10 by 6 is:", divide(10, 6))
-
-print("Please try again..!")
